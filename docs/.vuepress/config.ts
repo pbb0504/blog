6bbf26157bed420805576728f1127c50c4dc370f
panbinbin <1305355506@qq.com>
2021-04-12 23:03:13 +0800

    add docs/.vuepress/.cache | .temp to .gitignore

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,13 +1,20 @@
-import { defineUserConfig } from 'vuepress'
-import type { DefaultThemeOptions } from 'vuepress'
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
 
 export default defineUserConfig<DefaultThemeOptions>({
-  lang: 'zh-CN',
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base: '/blog/',
+  lang: "zh-CN",
+  title: "Hello VuePress",
+  description: "Just playing around",
+  base: "/blog/",
 
   themeConfig: {
-    logo: 'https://vuejs.org/images/logo.png',
+    logo: "https://vuejs.org/images/logo.png",
+    navbar: [
+      {
+        text: "guide",
+        link: "/guide/",
+      },
+      "/README.md",
+    ],
   },
-})
+});
---
+++ docs/guide/README.md
@@ -0,0 +1,2 @@
+# this is a first level title
+## this is a second level title
