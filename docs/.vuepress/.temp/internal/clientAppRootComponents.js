8eba6a98e2b77d9dfa728b41442aa72a96d9328f
panbinbin <1305355506@qq.com>
2021-04-12 16:48:22 +0800

    vuepress init

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'E:/Users/Desktop/blog/node_modules/@vuepress/plugin-back-to-top/lib/client/BackToTop.vue'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
