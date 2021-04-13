9cefa11f47162d7f45aeaec68255ed209bc8c11d
panbinbin <1305355506@qq.com>
2021-04-13 22:58:25 +0800

    frontmatter

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -12,6 +12,14 @@ export default defineUserConfig<DefaultThemeOptions>({
     search: true,
     searchMaxSuggestions: 10,
     smoothScroll: true,
+    // 它将被用作 仓库链接 的链接。仓库链接 将会显示为导航栏的最后一个元素。
+    repo: 'pbb0504/blog',
+    // 它将被用作 仓库链接 的文字。仓库链接 将会显示为导航栏的最后一个元素。
+    repoLabel: 'github',
+    /* // 编辑此页 链接的文字。
+    editLinkText: '编辑此页面',
+    // 最近更新时间戳 标签的文字。
+    lastUpdatedText: '最近更新', */
     navbar: [
       {
         text: "guide",
@@ -22,5 +30,14 @@ export default defineUserConfig<DefaultThemeOptions>({
         link: '/README.md'
       }
     ],
+    sidebar: {
+      '/guide/': [
+        {
+          isGroup: true,
+          text: 'guide',
+          children: ['/guide/demo1.md','/guide/demo2.md'],
+        },
+      ]
+    }
   },
 });
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,16 @@
 ---
 home: true
-heroImage: /hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
-actionLink: /guide/
+# heroImage: https://vuejs.org/images/logo.png
+# heroAlt: logo
+heroText: 潘彬彬的博客
+tagline: 时间的记录
+actions:
+- text: 快速上手
+  link: /guide/getting-started.md
+  type: primary
+- text: 关于我
+  link: /guide/AboutMe.md
+  type: secondry
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
@@ -12,5 +18,5 @@ features:
   details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
 - title: 高性能
   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+footer: MIT Licensed | Copyright © 2021-present Pan Binbin
 ---
